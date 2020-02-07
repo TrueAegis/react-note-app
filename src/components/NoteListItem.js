0d99efce63a3e378004b87d7ade8120047480175
tparsons008 <tparsons008@gmail.com>
2020-02-07 09:48:56 -0600

    Adds better date & time

--- src/components/NoteListItem.js
+++ src/components/NoteListItem.js
@@ -1,16 +1,17 @@
 /* eslint-disable react/no-typos */
 import React from "react";
 import PropTypes from "prop-types";
+import dayjs from "dayjs";
 
 export default function NoteListPage(props) {
-    const { id, text, dateTimeText, onClick } = props;
+    const { id, text, createdAt, onClick } = props;
     const truncate = (text) => text.length > 200 ? `${text.substring(0, 200)}...` : text;
     
     return (
         //jsx
         <div className="listItem" onClick = {() => onClick(id)}>
                 <p>{truncate(text)}</p>
-                <p>{dateTimeText}</p>
+                <p>{dayjs(createdAt).format("h:m a on M/D/YYYY")}</p>
             
         </div>
     );
@@ -19,6 +20,6 @@ export default function NoteListPage(props) {
 NoteListPage.PropTypes={
     id : PropTypes.string.isRequired,
     text : PropTypes.string.isRequired,
-    dateTimeText : PropTypes.string.isRequired,
+    createdAt : PropTypes.string.isRequired,
     onClick : PropTypes.func
 }
--- src/components/NoteListPage.js
+++ src/components/NoteListPage.js
@@ -12,14 +12,17 @@ export default function NoteListPage() {
 
     return (
         //JSX
-        <div className="list">
-            <NoteListItem
-                id="123"
-                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pharetra luctus est rutrum dapibus. Proin sit amet eleifend sapien. Proin eu nulla facilisis, ullamcorper magna eu, ullamcorper tellus. Ut faucibus nisl at lectus blandit vehicula a in lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec felis libero, dictum eget ante varius, feugiat malesuada sapien. Nunc sed lectus nec tellus tincidunt pellentesque ultrices at lectus. Mauris porttitor pharetra aliquam. Sed nisl enim, aliquet vitae sapien at, sagittis hendrerit metus. Nunc dictum gravida lectus, et egestas mi accumsan eu. Cras tempus, diam at placerat faucibus, nisi nulla rutrum lectus, nec iaculis eros lectus id orci. Etiam consequat ligula ex, et vulputate magna vulputate ac. Mauris malesuada cursus arcu et facilisis. Nam dapibus nulla erat, maximus gravida neque faucibus in. Quisque sollicitudin enim lorem, eu porttitor erat suscipit nec.
-                Proin a eros ultricies, dapibus lacus nec, dignissim purus. Aenean eget orci sed nunc tempus tempor vel id tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse tincidunt, nunc a sollicitudin vestibulum, diam felis accumsan risus, sed efficitur libero nisi in leo. Duis non semper turpis. Donec vitae dictum nibh. Etiam dictum pulvinar pulvinar. Proin ultricies nisl eu leo aliquam semper. Suspendisse aliquam."
-                dateTimeText="1/25/2020 5:00PM"
-                onClick={handleListItemClick}
-            />
+        <div className = "page">
+            <h1>Note List</h1>
+            <div className="list">
+                <NoteListItem
+                    id="123"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pharetra luctus est rutrum dapibus. Proin sit amet eleifend sapien. Proin eu nulla facilisis, ullamcorper magna eu, ullamcorper tellus. Ut faucibus nisl at lectus blandit vehicula a in lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec felis libero, dictum eget ante varius, feugiat malesuada sapien. Nunc sed lectus nec tellus tincidunt pellentesque ultrices at lectus. Mauris porttitor pharetra aliquam. Sed nisl enim, aliquet vitae sapien at, sagittis hendrerit metus. Nunc dictum gravida lectus, et egestas mi accumsan eu. Cras tempus, diam at placerat faucibus, nisi nulla rutrum lectus, nec iaculis eros lectus id orci. Etiam consequat ligula ex, et vulputate magna vulputate ac. Mauris malesuada cursus arcu et facilisis. Nam dapibus nulla erat, maximus gravida neque faucibus in. Quisque sollicitudin enim lorem, eu porttitor erat suscipit nec.
+                    Proin a eros ultricies, dapibus lacus nec, dignissim purus. Aenean eget orci sed nunc tempus tempor vel id tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse tincidunt, nunc a sollicitudin vestibulum, diam felis accumsan risus, sed efficitur libero nisi in leo. Duis non semper turpis. Donec vitae dictum nibh. Etiam dictum pulvinar pulvinar. Proin ultricies nisl eu leo aliquam semper. Suspendisse aliquam."
+                    dateTimeText="1/25/2020 5:00PM"
+                    onClick={handleListItemClick}
+                />
+            </div>
         </div>
     );
 
