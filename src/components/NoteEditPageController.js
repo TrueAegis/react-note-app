73ceb51271b4fd298f10cbf694f8beaba0b3d5df
tparsons008 <tparsons008@gmail.com>
2020-05-03 09:03:52 -0500

    Adds save not function

--- src/components/NoteEditPageController.js
+++ src/components/NoteEditPageController.js
@@ -1,15 +1,39 @@
 import React from "react";
 import { useParams } from "react-router";
 import { useHistory } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
 import NoteEditPage from "./NoteEditPage";
 import useNotes from "../hooks/useNotes";
 
+const GET_ONE_NOTE = gql`
+query note($id: ID!){
+    note(id: $id) {
+        id
+        createdAt
+        isArchived
+        text
+    }
+}
+`
 export default function NoteEditPageController() {
     const { id } = useParams();
     const history = useHistory();
-    const { notes, deleteNote, updateNote, archiveNote } = useNotes();
+    const { data, error, loading } = useQuery(GET_ONE_NOTE, {
+        variables: {
+            id
+        }
+    });
+    const { deleteNote, updateNote, archiveNote } = useNotes();
 
-    const selectedNote = notes.find((note) => note.id === id);
+    if (loading){
+        return "Loading...";
+    }
+
+    if (error){
+        return `${error}`;
+    }
+
+    const selectedNote = data && data.note;
     if (!selectedNote) return null;
 
     function handleOnArchive() {
--- src/components/NoteListPage.js
+++ src/components/NoteListPage.js
@@ -15,34 +15,61 @@ import {
 import { add, funnel } from "ionicons/icons";
 import { useHistory } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import NoteListItem from "./NoteListItem";
-import useNotes from "../hooks/useNotes";
 import "./NoteEditPage.module.css";
 
 const GET_NOTES = gql`
     {
-        notes(includeArchived: true){
-            id 
+        notes {
+            id
             createdAt
+            updatedAt
             isArchived
             text
+  }
+}
+`;
+
+const CREATE_NOTE = gql`
+    mutation createNote($note: CreateNoteInput!){
+        createNote(note: $note) {
+                id
+                createdAt
+                updatedAt
+                isArchived
+                text
+            }
         }
-    }
 `;
 
 export default function NoteListPage() {
-    const { data, error, loading } = useQuery(GET_NOTES); //First run { null, null, true }
-    const { createNote } = useNotes();
+    const [createNote] = useMutation(CREATE_NOTE, {
+        onCompleted(data) {
+            if(data && data.createNote) {
+                const id = data.createNote.id;
+                history.push(`./notes/edit/${id}`)
+            }
+        },
+        refetchQueries: [
+            {
+                query: GET_NOTES
+            }
+        ]
+    });
+    const { data, error, loading } = useQuery(GET_NOTES, {
+        pollInterval: 5000
+    }); //First run { null, null, true }
+    //const { createNote } = useNotes();
     const history = useHistory();
     const { t } = useTranslation();
     const [isArchived, setIsArchived] = useState(true);
 
-    if (loading){
+    if (loading) {
         return "Loading..."; //TODO: loading spinner
     }
 
-    if(error){
+    if (error) {
         return `${error}`; //Displays on page for now
     }
 
@@ -57,8 +84,13 @@ export default function NoteListPage() {
     }
 
     const handleNewNoteClick = () => {
-        const { id } = createNote();
-        history.push(`./notes/edit/${id}`)
+        createNote({
+            variables: {
+                note: {
+                    text: ""
+                }
+            }
+        });
     }
 
     return (
@@ -79,7 +111,7 @@ export default function NoteListPage() {
                         filteredNotes.map((note) => {
                             return (
                                 <NoteListItem
-                                    createdAt={note.createdAt}
+                                    createdAt={new Date(note.createdAt)}
                                     id={note.id}
                                     key={note.id}
                                     onClick={handleListItemClick}
